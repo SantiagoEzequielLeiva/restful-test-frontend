514bbb9688b6d4bd93dcc5cf25f6e09ae31aaed5
Santiago Leiva <santiagoeleiva@gmail.com>
2018-04-29 14:07:29 -0300

    Se agrega pantalla de usuarios + Router Outlet en app.component.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes, Router } from '@angular/router';
 import { AppComponent } from './app.component';
+import { UsuarioComponent } from './usuario/usuario.component';
 
 const appRoutes: Routes = [
-    /* { path: '', redirectTo: '/index', pathMatch: 'full' }, */
-    { path: '', component: AppComponent }
+    { path: '', redirectTo: '/usuarios', pathMatch: 'full' },
+    { path: 'index', component: AppComponent },
+    { path: 'usuarios', component: UsuarioComponent }
 ];
 
 @NgModule({
